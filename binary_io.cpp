fa9fc3ae63fb4fd7ab8757d0a3d0431adfb73709
William Dawn <wdawn730@gmail.com>
2017-08-17 09:27:00 -0400

    working on actual io

--- binary_io.cpp
+++ binary_io.cpp
@@ -2,45 +2,46 @@
 # include <fstream> // std::ifstream, etc.
 # include <sstream> // std::stringstream
 
-# include <string> // std::string
-# include <vector> // std::vector
+std::stringstream msg;
 
-std::vector<std::string> msg;
-std::stringstream myMsg;
-
+// TO-DO: write raiseWarn()
 void raiseFatal()
 {
   std::cout << std::string(80,'*') << '\n';
   for (int i{0}; i < 13; ++i)
     std::cout << "FATAL ";
   std::cout << std::string(2,'\n');
-  std::cout << myMsg.str();
+  std::cout << msg.str();
   std::cout << '\n';
   for (int i{0}; i < 13; ++i)
     std::cout << "FATAL ";
   std::cout << '\n';
   std::cout << std::string(80,'*') << '\n';
-  myMsg.str("");
-  myMsg.clear();
+  msg.str("");
+  msg.clear();
   exit(1);
-}
-
-void msgAdd(const char *letters)
-{
-  msg.push_back(std::string(letters));
 }
 
 int main()
 {
+  // TO-DO: this should be a command line argument
   std::string fname("ISOTXS.u235");
+  // open the file as a binary file
+  // TO-DO: look into endian-ness
   std::ifstream myFile (fname.c_str(), std::ios::in | std::ios::binary);
   if (!myFile)
   {
-    myMsg << "failed to open " << fname << '\n';
+    msg << "failed to open " << fname << '\n';
     raiseFatal();
   }
 
-
+  // begin actually reading the file
+  const int RECORD_DELIMITER_LENGTH = 4;
+  myFile.seekg(RECORD_DELIMITER_LENGTH, std::ios::cur);
+  myFile.read((char*) &buffer,1000);
+  for (int i{0}; i < 1000; ++i)
+    std::cout << buffer[i];
+  std::cout << '\n';
 
   myFile.close();
 
