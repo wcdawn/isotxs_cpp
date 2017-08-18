b2dedbe9c92db4f63762e29f698a15dac296d994
William Dawn <wdawn730@gmail.com>
2017-08-18 16:47:58 -0400

    parsing out main file

--- binary_io.cpp
+++ binary_io.cpp
@@ -5,78 +5,9 @@
 # include <array>
 # include <vector>
 
+# include "isotxs_struct.h"
 
 std::stringstream msg;
-const int holl_len{8};
-
-struct ISOTXS
-{
-  std::array<char,holl_len> hname;
-  std::array<std::array<char,holl_len>,2> huse;
-  int32_t ivers;
-
-  int32_t ngroup;
-  int32_t niso;
-  int32_t maxup;
-  int32_t maxdn;
-  int32_t maxord;
-  int32_t ichist;
-  int32_t nscmax;
-  int32_t nsblok;
-
-  std::array<std::array<char,holl_len>,12> hsetid;
-  std::vector<std::array<char,holl_len>> hisonm;
-  std::vector<float> chi;
-  std::vector<float> vel;
-  std::vector<float> emax;
-  float emin;
-  std::vector<int32_t> loca;
-
-  std::vector<std::vector<float>> chi_fw;
-  std::vector<int32_t> isspec;
-
-  std::array<char,holl_len> habsid;
-  std::array<char,holl_len> hident;
-  std::array<char,holl_len> hmat;
-  float amass;
-  float efiss;
-  float ecapt;
-  float temp;
-  float sigpot;
-  float adens;
-  int32_t kbr;
-  int32_t ichi;
-  int32_t ifis;
-  int32_t ialf;
-  int32_t inp;
-  int32_t in2n;
-  int32_t ind;
-  int32_t intritium;
-  int32_t ltot;
-  int32_t ltrn;
-  int32_t istrpd;
-  std::vector<int32_t> idsct;
-  std::vector<int32_t> lord;
-  std::vector<std::vector<int32_t>> jband;
-  std::vector<std::vector<int32_t>> ijj;
-
-  std::vector<std::vector<float>> strpl;
-  std::vector<std::vector<float>> stotpl;
-  std::vector<float> sngam;
-  std::vector<float> sfis;
-  std::vector<float> snutot;
-  std::vector<float> chiso;
-  std::vector<float> snalf;
-  std::vector<float> snp;
-  std::vector<float> sn2n;
-  std::vector<float> snd;
-  std::vector<float> snt;
-
-  std::vector<std::vector<float>> chiiso;
-  std::vector<float> isopec;
-
-  std::vector<std::vector<std::vector<float>>> scat;
-};
 
 // TO-DO: move these to another file
 void raiseFatal()
@@ -219,7 +150,6 @@ int main()
     iso_data.chi_fw[i].reserve(iso_data.ngroup);
   iso_data.isspec.reserve(iso_data.ngroup);
 
-  std::cout << "nscmax: " << iso_data.nscmax << '\n';
   iso_data.idsct.reserve(iso_data.nscmax);
   iso_data.lord.reserve(iso_data.nscmax);
   iso_data.jband.reserve(iso_data.nscmax);
@@ -245,6 +175,7 @@ int main()
   }
   recordChange(myFile);
   std::cout << "--- end 1D\n";
+  recordChange(myFile);
   for (unsigned int i{0}; i < iso_data.hsetid.size(); ++i)
   {
     holl_arrayRead(myFile,iso_data.hsetid[i]);
@@ -260,7 +191,6 @@ int main()
   vectorRead(myFile,iso_data.vel);
   vectorRead(myFile,iso_data.emax);
   bRead(myFile,iso_data.emin);
-  recordChange(myFile);
   vectorRead(myFile,iso_data.loca);
   recordChange(myFile);
   std::cout << "--- end 2D\n";
@@ -382,6 +312,7 @@ int main()
           iso_data.scat[j][k].reserve(iso_data.lord[j]);
         matrixRead(myFile,iso_data.scat[j]);
         recordChange(myFile);
+        std::cout << "--- end 7D\n";
       }
     }
 
